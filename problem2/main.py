db1ab87a3b6f79aaa4671aec9d89ee1fb367b49c
rismanovic <rismanovi24@gmail.com>
2024-05-05 09:43:33 +0700

    commit to vscode

--- problem2/main.py
+++ problem2/main.py
@@ -4,3 +4,9 @@ tinggi = 25.0
 
 
 # kode disini
+input_alas = input("Enter input_alas =  ")
+input_tinggi = input("Enter input_tinggi =  ")
+
+area = (alas * tinggi) /2
+
+print("Triangle Area =  ", round(area,2))
--- problem3/main.py
+++ problem3/main.py
@@ -4,3 +4,11 @@ r = 4.0
 
 
 # kode disini
+#area of tube
+input_T = input("Enter input_T =  ")
+input_r = input("Enter input_r =  ")
+
+phi = 3.14
+area = (2 * phi * r * r) + (2 * phi * r * T)
+
+print("Tube Area =  ", round(area,2))
